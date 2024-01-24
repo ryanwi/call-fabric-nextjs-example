6d35bac49920fc7fe2c4d20e30820317d32e84be
Ryan Williams <ryan.williams@signalwire.com>
2024-01-24 08:24:52 -0800

    fix: navbar needs children type

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,10 @@
-export default function NavBar({ children }) {
+import React, { ReactNode } from 'react';
+
+interface NavBarProps {
+ children: ReactNode;
+}
+
+export default function NavBar({ children }: NavBarProps) {
   return (
      <div className="navbar bg-neutral text-neutral-content">
        {children}
