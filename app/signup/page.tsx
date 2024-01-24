d00c2cc0401e71a0c8e700c92d50481314fdcaac
Ryan Williams <ryan.williams@signalwire.com>
2024-01-24 08:20:37 -0800

    initial signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,10 +1,50 @@
-import { FormEvent } from 'react'
+import { redirect } from 'next/navigation';
 
+interface CreateFabricSubscriberRequest {
+  email: string;
+  password: string;
+  first_name?: string;
+  last_name?: string;
+  display_name?: string;
+  job_title?: string;
+  time_zone?: string;
+  country?: string;
+  region?: string;
+  company_name?: string;
+}
+ 
 export default function Page() {
-  async function onSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
+  async function createSubscriber(formData: FormData) {
+    'use server'
 
-    const formData = new FormData(event.currentTarget)
+    const subscriber: CreateFabricSubscriberRequest = {
+      email: formData.get('email') as string,
+      password: formData.get('password') as string,
+      first_name: formData.get('firstName') as string,
+      last_name: formData.get('lastName') as string,
+      job_title: formData.get('jobTitle') as string,
+      company_name: formData.get('companyName') as string,
+    }
+
+    const authString = Buffer.from(
+      `${process.env.SIGNALWIRE_PROJECT}:${process.env.SIGNALWIRE_API_TOKEN}`
+    ).toString('base64')
+    console.log("authString =", authString)
+    const response = await fetch(`${process.env.SIGNALWIRE_API_URL}/api/fabric/subscribers`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Basic ${authString}`,
+      },
+      body: JSON.stringify(subscriber),
+    })
+
+    if (response.ok) {
+      redirect('/api/auth/signin')
+    } else {
+      const errorBody = await response.text()
+      console.log('Error Body:', errorBody)
+    }
   }
 
   return (
@@ -12,87 +52,82 @@ export default function Page() {
       <div className="hero-content">
         <div className="max-w-md">
           <h1 className="text-4xl mb-4 font-extrabold">Sign Up</h1>
-          
-            <div role="alert" className="alert alert-warning">
-              <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
-              <span>errors</span>
-            </div>
-        
+
           <div>
-            <form method="POST" className="shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <form action={createSubscriber} className="shadow-md rounded px-8 pt-6 pb-8 mb-4">
               <div className="mb-4">
                 <label>
                   First Name:
-                  <input name="firstName" type="text" required className="input input-bordered w-full" />
+                  <input
+                    name="firstName"
+                    type="text"
+                    required
+                    className="input input-bordered w-full"
+                  />
                 </label>
-                  <div className="text-red-500 flex items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                      <path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495ZM10 5a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 10 5Zm0 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clip-rule="evenodd" />
-                    </svg>
-                  </div>              
               </div>
               <div className="mb-4">
                 <label>
                   Last Name:
-                  <input name="lastName" type="text" required className="input input-bordered w-full" />
+                  <input
+                    name="lastName"
+                    type="text"
+                    required
+                    className="input input-bordered w-full"
+                  />
                 </label>
-                  <div className="text-red-500 flex items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                      <path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495ZM10 5a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 10 5Zm0 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clip-rule="evenodd" />
-                    </svg>
-                  </div>              
               </div>
               <div className="mb-4">
                 <label>
                   Email:
-                  <input name="email" type="email" required className="input input-bordered w-full" />
+                  <input
+                    name="email"
+                    type="email"
+                    required
+                    className="input input-bordered w-full"
+                  />
                 </label>
-                  <div className="text-red-500 flex items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                      <path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495ZM10 5a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 10 5Zm0 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clip-rule="evenodd" />
-                    </svg>
-                  </div>              
               </div>
               <div className="mb-4">
                 <label>
                   Password:
-                  <input name="password" type="text" required className="input input-bordered w-full" />
+                  <input
+                    name="password"
+                    type="text"
+                    required
+                    className="input input-bordered w-full"
+                  />
                 </label>
-                  <div className="text-red-500 flex items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                      <path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495ZM10 5a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 10 5Zm0 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clip-rule="evenodd" />
-                    </svg>
-                  </div>
               </div>
               <div className="mb-4">
                 <label>
                   Company:
-                  <input name="companyName" type="text" className="input input-bordered w-full" />
+                  <input
+                    name="companyName"
+                    type="text"
+                    className="input input-bordered w-full"
+                  />
                 </label>
-                  <div className="text-red-500 flex items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                      <path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495ZM10 5a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 10 5Zm0 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clip-rule="evenodd" />
-                    </svg>
-                  </div>
               </div>
               <div className="mb-4">
                 <label>
                   Job Title:
-                  <input name="jobTitle" type="text" className="input input-bordered w-full" />
+                  <input
+                    name="jobTitle"
+                    type="text"
+                    className="input input-bordered w-full"
+                  />
                 </label>
-                  <div className="text-red-500 flex items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                      <path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495ZM10 5a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 10 5Zm0 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clip-rule="evenodd" />
-                    </svg>
-                  </div>
               </div>
               <div className="">
-                <button type="submit" className="btn btn-primary">Sign Up</button>
+                <button type="submit" className="btn btn-primary">
+                  Sign Up
+                </button>
               </div>
             </form>
           </div>
         </div>
       </div>
-    </div>    
+    </div>
   )
 }
