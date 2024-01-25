601a6d118d587e76b92a727643a6400fb7dcdec8
Ryan Williams <ryan.williams@signalwire.com>
2024-01-24 23:32:10 -0800

    Add calls

---
+++ app/calls/page.tsx
@@ -0,0 +1,20 @@
+import { auth } from "auth"
+
+export default async function Page()  {
+  const session = await auth()
+
+  if (session?.user) {
+    return (
+      <div>
+        <h1 className="text-4xl mb-4 mt-4 font-extrabold">Calls</h1>
+        
+      </div>
+    )
+  }
+
+  return (
+    <p>
+      No session data, please <em>Sign In</em> first.
+    </p>
+  )
+}
