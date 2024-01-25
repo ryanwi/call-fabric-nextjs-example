a1461699a14cab87a3ee489c32a6666b76551017
Ryan Williams <ryan.williams@signalwire.com>
2024-01-24 23:30:09 -0800

    Add profile page

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -7,24 +7,7 @@ export default async function Page()  {
     return (
       <div>
         <h1 className="text-4xl mb-4 mt-4 font-extrabold">Rooms</h1>
-
-        <div className="w-full space-y-2 overflow-auto">
-          <h2 className="text-xl font-bold">Current Session Data</h2>
-          {Object.keys(session.user).length > 3 ? (
-            <p>
-              In this example, the whole session object is passed to the page,
-              including the raw user object. Our recommendation is to{" "}
-              <em>only pass the necessary fields</em> to the page, as the raw user
-              object may contain sensitive information.
-            </p>
-          ) : (
-            <p>
-              In this example, only some fields in the user object is passed to
-              the page to avoid exposing sensitive information.
-            </p>
-          )}
-          <pre>{JSON.stringify(session, null, 2)}</pre>
-        </div>
+        
       </div>
     )
   }
