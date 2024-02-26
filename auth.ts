8fa969b50ff2dca84bb983d9705bc0dcc1a334e6
Ryan Williams <ryan.williams@signalwire.com>
2024-02-25 17:22:09 -0800

    feat: Refresh access token when expired

--- auth.ts
+++ auth.ts
@@ -1,5 +1,7 @@
 import NextAuth, { type Session } from "next-auth";
 import type { NextAuthConfig } from "next-auth"
+import { type TokenSet } from "@auth/core/types"
+import { JWT } from "@auth/core/jwt";
 
 export const config = {
   theme: {
@@ -43,6 +45,7 @@ export const config = {
       return session
     },    
     async jwt({ token, account, user }) {
+      // Initial sign in
       if (account && user) {
         // Persist the OAuth access_token and or the user id to the token right after signin
         // token.name = `${user.firstName} ${user.lastName}`
@@ -50,16 +53,48 @@ export const config = {
         token.accessToken = account.access_token
         token.refreshToken = account.refresh_token
         token.expiresAt = Math.floor(Date.now() / 1000 + (account.expires_in || 0))
-      // } else if (Date.now() < token.expiresAt * 1000) {
-      // isValidToken
-      //   return token
-      // } else {
-      //   // If the access token has expired, try to refresh it
+        return token
       }
 
-      return token
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < token.expiresAt * 1000) {
+        return token
+      }
+
+      // If the access token has expired, try to refresh it
+      const tokens = await refreshAccessToken(token)
+      return {
+        ...token,
+        accessToken: tokens.access_token,
+        expiresAt: Math.floor(Date.now() / 1000 + tokens.expires_in),
+      };        
+
+      // return token
     },
   },
 } satisfies NextAuthConfig
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config)
+
+async function refreshAccessToken(token) {
+  console.log("refreshAccessToken", token)
+
+  const response = await fetch(process.env.SIGNALWIRE_ACCESS_TOKEN_URL || '', {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams({
+      client_id: process.env.SIGNALWIRE_CLIENT_ID || '',
+      client_secret: process.env.SIGNALWIRE_CLIENT_SECRET || '',
+      grant_type: 'refresh_token',
+      refresh_token: token.refreshToken,
+    }),
+    method: 'POST',
+  });
+
+  if (!response.ok) {
+    console.error('Error refreshing access token', await response.text());
+    throw new Error('RefreshAccessTokenError');
+  }
+
+  const data = await response.json();
+  return data;
+}
